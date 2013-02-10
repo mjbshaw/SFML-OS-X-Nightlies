112fc4516835618f7ca8b1d4ecc4b765a257d014
Michael Bradshaw <mjbshaw@gmail.com>
2013-02-10 15:13:38 -0700

    Add initial build files

---
+++ build_all.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+# This build script generates various permutations of the targets below:
+# OS X Deployment Target: 10.5 (min version end users must have)
+# OS X Base SDK Versions: 10.7 and 10.8
+# Architectures: i386 and x86_64 (that is, x86 32-bit and 64-bit, universal)
+# Compilers: clang++ and g++
+# C++ versions: C++03 and C++11
+# Library types: Framekworks and Dynamic Libraries (dylibs)
+
+# Base SDK locations:
+# 10.5: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.5.sdk
+# 10.6: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk
+# 10.7: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk
+# 10.8: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk
+
+# Update SFML to the latest version
+cd SFML
+
+git fetch
+git rebase
+cd ..
+
+export sfml_src_dir="`pwd`/SFML"
+
+cd bin
+rm -rf *
+cd ..
+
+# Folder naming scheme example:
+# sfml_osx_10.7base_for_g++_and_c++03_deploys_to_10.5
+
+# If using 10.8 base SDK, the target must be 10.6 or later (with g++)
+# if using 10.7 base SDK, the target may be 10.5 (with g++)
+# I'm unable to get 10.6/10.5 base SDKs working without Xcode 3, which I don't really want to install right now... maybe later
+
+# Start building!
+export cxx_cmp='g++'
+export c_cmp='gcc'
+export cpp_ver='c++03'
+export base='10.7'
+export deploy='10.5'
+export extra_flags=
+./single_build.sh
+
+export cxx_cmp='g++'
+export c_cmp='gcc'
+export cpp_ver='c++03'
+export base='10.8'
+export deploy='10.6'
+export extra_flags=
+./single_build.sh
+
+
+
+export cxx_cmp='clang++'
+export c_cmp='clang'
+export cpp_ver='c++11'
+export base='10.8'
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++'
+./single_build.sh
+
+export cxx_cmp='clang++'
+export c_cmp='clang'
+export cpp_ver='c++11'
+export base='10.7'
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++'
+./single_build.sh
+
+export cxx_cmp='clang++'
+export c_cmp='clang'
+export cpp_ver='c++03'
+export base='10.8'
+export deploy='10.5'
+export extra_flags=
+./single_build.sh
+
+export cxx_cmp='clang++'
+export c_cmp='clang'
+export cpp_ver='c++03'
+export base='10.7'
+export deploy='10.5'
+export extra_flags=
+./single_build.sh
---
+++ single_build.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+# Contents:
+# frameworks
+#   root/Library/Frameworks/
+# dylibs
+#   root/usr/local/include/
+#   root/usr/local/lib/
+# documentation
+#    root/usr/local/share/
+# sndfile
+#    root/Library/Frameworks/
+# templates
+#    root/Library/Developer/Xcode/Templates/
+
+# for templates:
+# cp -r SFML/tools/xcode/templates/SFML templates/root/Library/Developer/Xcode/Templates/
+
+# for sndfile:
+# cp -r SFML/extlibs/libs-osx/Frameworks/sndfile.framework sndfile/root/Library/Frameworks/
+
+# for documentation
+# cp -r doc/html documentation/root/usr/local/share/SFML/doc
+# cp  SFML/license.txt documentation/root/usr/local/share/SFML
+# cp  SFML/readme.txt documentation/root/usr/local/share/SFML
+
+# for dylibs:
+# cp lib/* dylibs/root/usr/local/lib/
+# cp -r SFML/include/SFML dylibs/root/usr/local/include/
+
+# for frameworks:
+# cp -r SFML.framework frameworks/root/Library/Frameworks/
+# cp -r lib/* frameworks/root/Library/Frameworks/
+
+dir="sfml_osx_${base}base_for_${cxx_cmp}_and_${cpp_ver}_deploys_to_${deploy}"
+mkdir "$dir"
+cd "$dir"
+
+mkdir "$dir"
+install_dir="`pwd`/$dir"
+
+mkdir frameworks
+cd frameworks
+
+# Build as frameworks
+cmake -G "Unix Makefiles" \
+      -DCMAKE_OSX_ARCHITECTURES='i386;x86_64' \
+      -DCMAKE_CXX_COMPILER="/usr/bin/$cxx_cmp" \
+      -DCMAKE_C_COMPILER="/usr/bin/$c_cmp" \
+      -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk" \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET="$deploy" \
+      -DSFML_BUILD_FRAMEWORKS="ON" \
+      -DSFML_INSTALL_XCODE4_TEMPLATES="ON" \
+      -DSFML_BUILD_DOC="ON" \
+      $extra_flags \
+      "$sfml_src_dir"
+
+make -j8
+
+# for templates:
+ditto "$sfml_src_dir/tools/xcode/templates/SFML" "$install_dir/templates/root/Library/Developer/Xcode/Templates/SFML"
+
+# for sndfile:
+ditto "$sfml_src_dir/extlibs/libs-osx/Frameworks/sndfile.framework" "$install_dir/sndfile/root/Library/Frameworks/sndfile.framework"
+
+# for documentation
+ditto "doc/html" "$install_dir/documentation/root/usr/local/share/SFML/doc/html"
+cp  "$sfml_src_dir/license.txt" "$install_dir/documentation/root/usr/local/share/SFML"
+cp  "$sfml_src_dir/readme.txt" "$install_dir/documentation/root/usr/local/share/SFML"
+
+# for frameworks:
+ditto "SFML.framework" "$install_dir/frameworks/root/Library/Frameworks/SFML.framework"
+ditto "lib" "$install_dir/frameworks/root/Library/Frameworks/"
+
+cd ..
+
+mkdir dylibs
+cd dylibs
+
+# Build as dylibs
+cmake -G "Unix Makefiles" \
+      -DCMAKE_OSX_ARCHITECTURES='i386;x86_64' \
+      -DCMAKE_CXX_COMPILER="/usr/bin/$cxx_cmp" \
+      -DCMAKE_C_COMPILER="/usr/bin/$c_cmp" \
+      -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk" \
+      -DCMAKE_OSX_DEPLOYMENT_TARGET="$deploy" \
+      $extra_flags \
+      "$sfml_src_dir"
+
+make -j8
+
+
+# for dylibs:
+ditto "lib" "$install_dir/dylibs/root/usr/local/lib/"
+ditto "$sfml_src_dir/include/SFML" "$install_dir/dylibs/root/usr/local/include/SFML"
+
+cd ..
+
+tar -cjf "${dir}.tar.bz2" "$dir"
+
+cp "${dir}.tar.bz2" ../bin
+
+cd ..
