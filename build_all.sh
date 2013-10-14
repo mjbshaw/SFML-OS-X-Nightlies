fb61b3d0ac7b916589501664e7459c5aa2a72f68
Michael Bradshaw <mjbshaw@gmail.com>
2013-10-14 09:38:07 -0600

    Update for OS X 10.9 and Xcode 5

--- build_all.sh
+++ build_all.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 # This build script generates various permutations of the targets below:
-# OS X Deployment Target: 10.5 (min version end users must have)
-# OS X Base SDK Versions: 10.7 and 10.8
+# OS X Deployment Target: 10.7 (min version end users must have)
+# OS X Base SDK Versions: 10.8 and 10.9
 # Architectures: i386 and x86_64 (that is, x86 32-bit and 64-bit, universal)
-# Compilers: clang++ and g++
+# Compilers: clang++
 # C++ versions: C++03 and C++11
 # Library types: Framekworks and Dynamic Libraries (dylibs)
 
 # Base SDK locations:
-# 10.5: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.5.sdk
-# 10.6: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk
-# 10.7: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk
 # 10.8: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.8.sdk
+# 10.8: /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk
 
 # Update SFML to the latest version
 cd SFML
@@ -34,29 +32,44 @@ git rebase master
 rm -rf bin/*
 
 # Folder naming scheme example:
-# sfml_osx_10.7base_for_g++_and_c++03_deploys_to_10.5
-
-# If using 10.8 base SDK, the target must be 10.6 or later (with g++)
-# if using 10.7 base SDK, the target may be 10.5 (with g++)
-# I'm unable to get 10.6/10.5 base SDKs working without Xcode 3, which I don't really want to install right now... maybe later
+# sfml_osx_10.8base_for_g++_and_c++03_deploys_to_10.7
 
 # Start building!
-export cxx_cmp='g++'
-export c_cmp='gcc'
-export cpp_ver='c++03'
-export base='10.7'
-export deploy='10.5'
-export extra_flags=
+export cxx_cmp='clang++'
+export c_cmp='clang'
+export cpp_ver='c++11'
+export base='10.8'
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++'
+export extra_name=
 ./single_build.sh
 
-export cxx_cmp='g++'
-export c_cmp='gcc'
+export cxx_cmp='clang++'
+export c_cmp='clang'
+export cpp_ver='c++11'
+export base='10.9'
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++'
+export extra_name=
+./single_build.sh
+
+export cxx_cmp='clang++'
+export c_cmp='clang'
 export cpp_ver='c++03'
 export base='10.8'
-export deploy='10.6'
-export extra_flags=
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libstdc++ -DCMAKE_C_FLAGS=-stdlib=libstdc++'
+export extra_name=
 ./single_build.sh
 
+export cxx_cmp='clang++'
+export c_cmp='clang'
+export cpp_ver='c++03'
+export base='10.9'
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libstdc++ -DCMAKE_C_FLAGS=-stdlib=libstdc++'
+export extra_name=
+./single_build.sh
 
 
 export cxx_cmp='clang++'
@@ -64,31 +77,35 @@ export c_cmp='clang'
 export cpp_ver='c++11'
 export base='10.8'
 export deploy='10.7'
-export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++ -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -flto -DNDEBUG" -DCMAKE_C_FLAGS_RELEASE="-Ofast -flto -DNDEBUG"'
+export extra_name="_Ofast_and_flto"
 ./single_build.sh
 
 export cxx_cmp='clang++'
 export c_cmp='clang'
 export cpp_ver='c++11'
-export base='10.7'
+export base='10.9'
 export deploy='10.7'
-export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libc++ -DCMAKE_C_FLAGS=-stdlib=libc++ -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -flto -DNDEBUG" -DCMAKE_C_FLAGS_RELEASE="-Ofast -flto -DNDEBUG"'
+export extra_name="_Ofast_and_flto"
 ./single_build.sh
 
 export cxx_cmp='clang++'
 export c_cmp='clang'
 export cpp_ver='c++03'
 export base='10.8'
-export deploy='10.5'
-export extra_flags=
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libstdc++ -DCMAKE_C_FLAGS=-stdlib=libstdc++ -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -flto -DNDEBUG" -DCMAKE_C_FLAGS_RELEASE="-Ofast -flto -DNDEBUG"'
+export extra_name="_Ofast_and_flto"
 ./single_build.sh
 
 export cxx_cmp='clang++'
 export c_cmp='clang'
 export cpp_ver='c++03'
-export base='10.7'
-export deploy='10.5'
-export extra_flags=
+export base='10.9'
+export deploy='10.7'
+export extra_flags='-DCMAKE_CXX_FLAGS=-stdlib=libstdc++ -DCMAKE_C_FLAGS=-stdlib=libstdc++ -DCMAKE_CXX_FLAGS_RELEASE="-Ofast -flto -DNDEBUG" -DCMAKE_C_FLAGS_RELEASE="-Ofast -flto -DNDEBUG"'
+export extra_name="_Ofast_and_flto"
 ./single_build.sh
 
 d=`date "+%Y-%m-%d"`
--- single_build.sh
+++ single_build.sh
@@ -1,38 +1,6 @@
 #!/bin/sh
 
-# Contents:
-# frameworks
-#   root/Library/Frameworks/
-# dylibs
-#   root/usr/local/include/
-#   root/usr/local/lib/
-# documentation
-#    root/usr/local/share/
-# sndfile
-#    root/Library/Frameworks/
-# templates
-#    root/Library/Developer/Xcode/Templates/
-
-# for templates:
-# cp -r SFML/tools/xcode/templates/SFML templates/root/Library/Developer/Xcode/Templates/
-
-# for sndfile:
-# cp -r SFML/extlibs/libs-osx/Frameworks/sndfile.framework sndfile/root/Library/Frameworks/
-
-# for documentation
-# cp -r doc/html documentation/root/usr/local/share/SFML/doc
-# cp  SFML/license.txt documentation/root/usr/local/share/SFML
-# cp  SFML/readme.txt documentation/root/usr/local/share/SFML
-
-# for dylibs:
-# cp lib/* dylibs/root/usr/local/lib/
-# cp -r SFML/include/SFML dylibs/root/usr/local/include/
-
-# for frameworks:
-# cp -r SFML.framework frameworks/root/Library/Frameworks/
-# cp -r lib/* frameworks/root/Library/Frameworks/
-
-dir="sfml_osx_${base}base_for_${cxx_cmp}_and_${cpp_ver}_deploys_to_${deploy}"
+dir="sfml_osx_${base}base_for_${cxx_cmp}_and_${cpp_ver}_deploys_to_${deploy}${extra_name}"
 mkdir "$dir"
 cd "$dir"
 
@@ -49,33 +17,98 @@ cmake -G "Unix Makefiles" \
       -DCMAKE_C_COMPILER="/usr/bin/$c_cmp" \
       -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk" \
       -DCMAKE_OSX_DEPLOYMENT_TARGET="$deploy" \
+      -DOPENAL_INCLUDE_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenAL.framework/Headers" \
+      -DOPENAL_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenAL.framework" \
+      -DOPENGL_INCLUDE_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenGL.framework/Headers" \
+      -DOPENGL_gl_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenGL.framework" \
+      -DOPENGL_glu_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/AGL.framework" \
       -DSFML_BUILD_FRAMEWORKS="ON" \
       -DSFML_INSTALL_XCODE4_TEMPLATES="ON" \
       -DSFML_BUILD_DOC="ON" \
+      -DSFML_BUILD_EXAMPLES="ON" \
       $extra_flags \
       "$sfml_src_dir"
 
 make -j8
 
 # for templates:
-ditto "$sfml_src_dir/tools/xcode/templates/SFML" "$install_dir/templates/root/Library/Developer/Xcode/Templates/SFML"
+ditto "$sfml_src_dir/tools/xcode/templates/SFML" "$install_dir/templates/SFML"
+
+# for cmake:
+ditto "$sfml_src_dir/cmake/Modules/FindSFML.cmake" "$install_dir/cmake/Modules/"
 
 # for sndfile:
-ditto "$sfml_src_dir/extlibs/libs-osx/Frameworks/sndfile.framework" "$install_dir/sndfile/root/Library/Frameworks/sndfile.framework"
+ditto "$sfml_src_dir/extlibs/libs-osx/Frameworks/sndfile.framework" "$install_dir/extlibs/sndfile.framework"
+
+# for freetype:
+ditto "$sfml_src_dir/extlibs/libs-osx/Frameworks/freetype.framework" "$install_dir/extlibs/freetype.framework"
 
 # for documentation
-ditto "doc/html" "$install_dir/documentation/root/usr/local/share/SFML/doc/html"
-cp  "$sfml_src_dir/license.txt" "$install_dir/documentation/root/usr/local/share/SFML"
-cp  "$sfml_src_dir/readme.txt" "$install_dir/documentation/root/usr/local/share/SFML"
+ditto "doc/html" "$install_dir/doc/html"
+cp  "$sfml_src_dir/license.txt" "$install_dir/"
+cp  "$sfml_src_dir/readme.txt" "$install_dir/"
+
+# for examples
+ditto "examples/cocoa/cocoa.app" "$install_dir/examples/cocoa/cocoa.app"
+
+ditto "$sfml_src_dir/examples/ftp/ftp.cpp" "$install_dir/examples/ftp/Ftp.cpp"
+ditto "examples/ftp/ftp" "$install_dir/examples/ftp/ftp"
+
+ditto "$sfml_src_dir/opengl/OpenGL.cpp" "$install_dir/examples/opengl/OpenGL.cpp"
+ditto "$sfml_src_dir/opengl/resources/background.jpg" "$install_dir/examples/opengl/resources/background.jpg"
+ditto "$sfml_src_dir/opengl/resources/sansation.ttf" "$install_dir/examples/opengl/resources/sansation.ttf"
+ditto "$sfml_src_dir/opengl/resources/texture.jpg" "$install_dir/examples/opengl/resources/texture.jpg"
+ditto "examples/opengl/opengl" "$install_dir/examples/opengl/opengl"
+
+ditto "$sfml_src_dir/pong/Pong.cpp" "$install_dir/examples/pong/Pong.cpp"
+ditto "$sfml_src_dir/pong/resources/ball.wav" "$install_dir/examples/pong/resources/ball.wav"
+ditto "$sfml_src_dir/pong/resources/sansation.ttf" "$install_dir/examples/pong/resources/sansation.ttf"
+ditto "examples/pong/pong" "$install_dir/examples/pong/pong"
+
+ditto "$sfml_src_dir/shader/Effect.hpp" "$install_dir/examples/shader/Effect.hpp"
+ditto "$sfml_src_dir/shader/Shader.cpp" "$install_dir/examples/shader/Shader.cpp"
+ditto "$sfml_src_dir/shader/resources/background.jpg" "$install_dir/examples/shader/resources/background.jpg"
+ditto "$sfml_src_dir/shader/resources/blink.frag" "$install_dir/examples/shader/resources/blink.frag"
+ditto "$sfml_src_dir/shader/resources/blur.frag" "$install_dir/examples/shader/resources/blur.frag"
+ditto "$sfml_src_dir/shader/resources/devices.png" "$install_dir/examples/shader/resources/devices.png"
+ditto "$sfml_src_dir/shader/resources/edge.frag" "$install_dir/examples/shader/resources/edge.frag"
+ditto "$sfml_src_dir/shader/resources/pixelate.frag" "$install_dir/examples/shader/resources/pixelate.frag"
+ditto "$sfml_src_dir/shader/resources/sansation.ttf" "$install_dir/examples/shader/resources/sansation.ttf"
+ditto "$sfml_src_dir/shader/resources/sfml.png" "$install_dir/examples/shader/resources/sfml.png"
+ditto "$sfml_src_dir/shader/resources/storm.vert" "$install_dir/examples/shader/resources/storm.vert"
+ditto "$sfml_src_dir/shader/resources/text-background.png" "$install_dir/examples/shader/resources/text-background.png"
+ditto "$sfml_src_dir/shader/resources/wave.vert" "$install_dir/examples/shader/resources/wave.vert"
+ditto "examples/shader/shader" "$install_dir/examples/shader/shader"
+
+ditto "$sfml_src_dir/sockets/Sockets.cpp" "$install_dir/examples/sockets/Sockets.cpp"
+ditto "$sfml_src_dir/sockets/TCP.cpp" "$install_dir/examples/sockets/TCP.cpp"
+ditto "$sfml_src_dir/sockets/UDP.cpp" "$install_dir/examples/sockets/UDP.cpp"
+ditto "examples/sockets/sockets" "$install_dir/examples/sockets/sockets"
+
+ditto "$sfml_src_dir/sound/Sound.cpp" "$install_dir/examples/sound/Sound.cpp"
+ditto "$sfml_src_dir/sound/resources/canary.wav" "$install_dir/examples/sound/resources/canary.wav"
+ditto "$sfml_src_dir/sound/resources/orchestral.ogg" "$install_dir/examples/sound/resources/orchestral.ogg"
+ditto "examples/sound/sound" "$install_dir/examples/sound/sound"
+
+ditto "$sfml_src_dir/sound_capture/SoundCapture.cpp" "$install_dir/examples/sound-capture/SoundCapture.cpp"
+ditto "examples/sound_capture/sound-capture" "$install_dir/examples/sound-capture/sound-capture"
+
+ditto "$sfml_src_dir/voip/Client.cpp" "$install_dir/examples/voip/Client.cpp"
+ditto "$sfml_src_dir/voip/Server.cpp" "$install_dir/examples/voip/Server.cpp"
+ditto "$sfml_src_dir/voip/VoIP.cpp" "$install_dir/examples/voip/VoIP.cpp"
+ditto "examples/voip/voip" "$install_dir/examples/voip/voip"
+
+ditto "$sfml_src_dir/window/Window.cpp" "$install_dir/examples/window/Window.cpp"
+ditto "examples/window/window" "$install_dir/examples/window/window"
 
 # for frameworks:
-ditto "SFML.framework" "$install_dir/frameworks/root/Library/Frameworks/SFML.framework"
-ditto "lib" "$install_dir/frameworks/root/Library/Frameworks/"
+ditto "SFML.framework" "$install_dir/Frameworks/SFML.framework"
+ditto "lib" "$install_dir/Frameworks/"
 
 cd ..
 
-mkdir dylibs
-cd dylibs
+mkdir lib
+cd lib
 
 # Build as dylibs
 cmake -G "Unix Makefiles" \
@@ -84,19 +117,25 @@ cmake -G "Unix Makefiles" \
       -DCMAKE_C_COMPILER="/usr/bin/$c_cmp" \
       -DCMAKE_OSX_SYSROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk" \
       -DCMAKE_OSX_DEPLOYMENT_TARGET="$deploy" \
+      -DOPENAL_INCLUDE_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenAL.framework/Headers" \
+      -DOPENAL_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenAL.framework" \
+      -DOPENGL_INCLUDE_DIR="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenGL.framework/Headers" \
+      -DOPENGL_gl_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/OpenGL.framework" \
+      -DOPENGL_glu_LIBRARY="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${base}.sdk/System/Library/Frameworks/AGL.framework" \
       $extra_flags \
       "$sfml_src_dir"
 
 make -j8
 
-
 # for dylibs:
-ditto "lib" "$install_dir/dylibs/root/usr/local/lib/"
-ditto "$sfml_src_dir/include/SFML" "$install_dir/dylibs/root/usr/local/include/SFML"
+ditto "lib" "$install_dir/lib"
+ditto "$sfml_src_dir/include/SFML" "$install_dir/include/SFML"
 
 echo These binaries were built on `date "+%Y-%m-%d"` from SFML $sfml_rev > "$install_dir/version.txt"
 
 cd ..
+
+cp "../install.sh" "$install_dir/"
 
 tar -cjf "${dir}.tar.bz2" "$dir"
 
